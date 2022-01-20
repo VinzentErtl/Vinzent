d370e094c28525865a87733ddb8940e1efa24be6
vinzent <vinzent.ertl@htldornbirn.at>
2022-01-20 14:34:44 +0100

    java script test

---
+++ Test_2cWI/test_js/script.js
@@ -0,0 +1,11 @@
+
+for (let nummer = 1; nummer < 9; nummer++) {
+    console.log(nummer)
+}
+console.log("Das Wars!")
+
+
+
+for (let Zahl = 11; Zahl > -4; Zahl = Zahl - 2) {
+    console.log(Zahl)
+}
