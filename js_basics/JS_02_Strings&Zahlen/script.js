0f3c9cdc5fb366c54f790eb07feb68658b326304
vinzent <vinzent.ertl@htldornbirn.at>
2022-03-17 14:05:01 +0100

    fixs

---
+++ js_basics/JS_02_Strings&Zahlen/script.js
@@ -0,0 +1,19 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d ;
+console.log(result);
+
+//Texte STRINGS
+
+let Name = "Vinzent";
+console.log(Name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
