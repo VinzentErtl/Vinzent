88f135e3fffa7125b86e7c27ef95cba7d8eea493
vinzent <vinzent.ertl@htldornbirn.at>
2022-04-28 14:57:34 +0200

    fertig mit den aufgaben

---
+++ js_basics/JS_08_Switch_Statement/script3.js
@@ -0,0 +1,19 @@
+let random = Math.random() * 5 + 5
+
+let randomAsInteger = Math.round(random);
+
+console.log(randomAsInteger);
+
+if (randomAsInteger == 10) {
+    console.log("Ten");
+} else if (randomAsInteger == 9) {
+    console.log("Nine");
+} else if (randomAsInteger == 8) {
+    console.log("Eight");
+} else if (randomAsInteger == 7) {
+    console.log("Seven");
+} else if (randomAsInteger == 6) {
+    console.log("Six");
+} else if (randomAsInteger == 5) {
+    console.log("Five");
+}
