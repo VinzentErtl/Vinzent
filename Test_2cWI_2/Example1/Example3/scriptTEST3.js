b0db08ae26dd33b289426a43c089b8c0a8c04f4b
vinzent <vinzent.ertl@htldornbirn.at>
2022-06-09 14:37:48 +0200

    3

---
+++ Test_2cWI_2/Example1/Example3/scriptTEST3.js
@@ -0,0 +1,11 @@
+let random = Math.random() * 100;
+
+let randomAsInteger = Math.round(random);
+
+for (let index = 0; index < 10; index++) {
+    console.log(randomAsInteger);
+    randomAsInteger + output = output;
+}
+console.log("Die Summe der 10 Zufallszahlen ist" + output)
+
+
